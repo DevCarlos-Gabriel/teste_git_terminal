c07228780a23169eff2ebee6c3a5149c8f3aecac
Carlos Gabriel <carlosgabriel03082005@gmail.com>
2024-07-07 19:42:44 -0300

    zerando arquivo

--- main.rs
+++ main.rs
@@ -1,21 +1,4 @@
-#[derive(Debug)]
-struct CadPessoa
-{
-    nome: String,
-    idade: u32,
-    cidade: String,
-}
-
 fn main() 
 {
-    let pessoa_1 = CadPessoa
-    {
-        nome: String::from("Lur"),
-        idade: 25,
-        cidade: String::from("teste"),
-    };
-    
-    println!("Todas as informções que temos em pessoa_1 é {:?}", pessoa_1);
-    println!("teste 0001 para Marge");
-    println!("Agora testando o Rebase, Vamo lá");
+    println!("Print para teste de commit");
 }
